007bc242cf0a549d0b57da5ef1ead55fd3b8c1d2
Maxim Spiridonov <taijased@gmail.com>
2019-02-21 12:24:21 +0300

    Initial Commit

---
+++ FeedRecipe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FeedRecipe
+//
+//  Created by Maxim Spiridonov on 21/02/2019.
+//  Copyright © 2019 Maxim Spiridonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
